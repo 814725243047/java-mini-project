b1fa01865262f9f91d64ece1b7b06fe19b9e72ea
Gokulnath.G <814725243047@trp.srmtrichy.edu.in>
2026-09-23 16:18:42 +0530

    Add Student class with attributes and methods

---
+++ 4. Student.java
@@ -0,0 +1,67 @@
+package com.attendance.model;
+
+public class Student {
+
+    private String id;
+    private String name;
+    private String department;
+    private String className;
+    private String phone;
+
+    public Student() {
+    }
+
+    public Student(
+            String id,
+            String name,
+            String department,
+            String className,
+            String phone) {
+
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.className = className;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
